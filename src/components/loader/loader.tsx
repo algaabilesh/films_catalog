ec059741bf31eed749c9be5f58f34dcd58398fb4
Alga Abilesh <alga.abilesh@gmail.com>
2024-10-26 01:31:08 +0600

    fix: small fixes

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import styles from "./loader.module.css";
 
 const Loader: FC = () => {
--- src/components/no-results/no-results.tsx
+++ src/components/no-results/no-results.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import styles from "./no-results.module.css";
 
 const NoResults: FC = () => {
